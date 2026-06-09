eb55e53f39e7800ec8bdf2212f5cd4adb5e6d732
Rom8 <roman.garkavenko@ya.ru>
2026-06-10 01:18:37 +0300

    Добавить dev профиль. Читать отдельно имя и фамилию.

--- src/main/java/ru/rom8/rescue/volunteer_gateway_service/SimpleController.java
+++ src/main/java/ru/rom8/rescue/volunteer_gateway_service/SimpleController.java
@@ -6,16 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 @RestController
 @RequestMapping("/")
 public class SimpleController {
 
-    private final AtomicLong counter = new AtomicLong();
-
     @GetMapping("/")
     public String hello(@AuthenticationPrincipal OAuth2User oauth2User) {
-        return String.format("Привет, %s! (%s)", oauth2User.getName(), counter.incrementAndGet());
+        String name = String.format("%s %s",
+                oauth2User.getAttribute("first_name"),
+                oauth2User.getAttribute("last_name"));
+        return String.format("Привет, %s!", name);
     }
 }
